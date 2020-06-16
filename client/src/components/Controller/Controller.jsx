9c2927f0b479715d327ef6d1f1e821a9559b420e
mick <mickwarehime@gmail.com>
2020-06-15 23:29:17 -0700

    remove traling commas

--- client/src/components/Controller/Controller.jsx
+++ client/src/components/Controller/Controller.jsx
@@ -1,5 +1,5 @@
-import React, { Component, } from 'react';
-import { Views, } from '../../utils/constants';
+import React, { Component } from 'react';
+import { Views } from '../../utils/constants';
 import LobbyView from '../LobbyView/LobbyView';
 import GameView from '../GameView/GameView';
 
