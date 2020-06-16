aecd44a8a29667c82974375e6c3d7c526cb6ff92
mick <mickwarehime@gmail.com>
2020-06-15 23:30:37 -0700

    remove traling commas

--- client/src/components/LobbyView/LobbyView.jsx
+++ client/src/components/LobbyView/LobbyView.jsx
@@ -1,4 +1,4 @@
-import React, { Component, } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@material-ui/core';
 import { FormControl } from '@material-ui/core';
