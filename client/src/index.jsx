616da45114647d08ed825198367f84f1398a0088
mick <mickwarehime@gmail.com>
2020-06-17 22:40:06 -0700

    improved icons

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Controller from './components/Controller/Controller';
+import './fontawesome';
 
 ReactDOM.render(<Controller />, document.getElementById('root',),);
---
+++ server/icons.py
@@ -0,0 +1,7 @@
+ICONS = ['bowling-ball', 'trash', 'crow', 'anchor', 'atom', 'baby', 'bath',
+         'bomb', 'bone', 'cannabis', 'bug', 'car', 'cloud', 'coffee', 'couch', 'dog', 'fish',
+         'ghost', 'glass-martini-alt', 'hammer', 'hamburger', 'head-side-cough', 'hippo', 'heartbeat',
+         'horse-head', 'ice-cream', 'infinity', 'life-ring', 'lightbulb', 'magic', 'palette', 'pastafarianism',
+         'pepper-hot', 'people-carry', 'poop', 'radiation', 'recycle', 'rocket', 'running', 'shower', 'skull',
+         'skull-crossbones', 'smoking', 'socks', 'spider', 'syringe', 'thumbtack', 'toilet-paper', 'yin-yang', 'tooth',
+         'sad-cry', 'pray', 'prescription-bottle', 'egg', 'kiwi-bird', 'hot-tub', 'wind']
