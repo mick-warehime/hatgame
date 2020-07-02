0cc6958dcdc5f07528cda954676d4e81a8d67a2a
dvirk <dvir.kafri@gmail.com>
2020-07-01 21:22:04 -0700

    tweak types

--- app/model/game_state.py
+++ app/model/game_state.py
@@ -3,17 +3,17 @@
 The implementation is hidden from app.py.
 """
 from dataclasses import dataclass
-from typing import List
+from typing import Tuple
 
 
-@dataclass
+@dataclass(frozen=True)
 class GameState:
     name: str  # Label for the current game instance
-    team_0_players: List[str]
+    team_0_players: Tuple[str, ...]
     team_0_name: str
     team_0_score: int
     team_0_icon: str
-    team_1_players: List[str]
+    team_1_players: Tuple[str, ...]
     team_1_name: str
     team_1_score: int
     team_1_icon: str
